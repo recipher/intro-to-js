c5d725259f282fceda1e48c8494d9d39fe43389e
Johnny <johnny@johnnys-mini.home>
2021-04-08 13:08:42 +0100

    Added async/await tasks

---
+++ 4-async/1-promises/tasks/2.js
@@ -0,0 +1,16 @@
+// Rewrite this example code from the chapter Promises chaining 
+// using async/await instead of .then/catch:
+
+function loadJson(url) {
+  return fetch(url)
+    .then(response => {
+      if (response.status == 200) {
+        return response.json();
+      } else {
+        throw new Error(response.status);
+      }
+    });
+}
+
+loadJson('no-such-user.json')
+  .catch(alert); // Error: 404
---
+++ 4-async/1-promises/tasks/3.js
@@ -0,0 +1,14 @@
+// We have a “regular” function called f. 
+// How can you call the async function wait() and use its result inside of f?
+
+async function wait() {
+  await new Promise(resolve => setTimeout(resolve, 1000));
+
+  return 10;
+}
+
+function f() {
+  // ...what should you write here?
+  // we need to call async wait() and wait to get 10
+  // remember, we can't use "await"
+}
